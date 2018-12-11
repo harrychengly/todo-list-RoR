df193caed384983ebe91f0dbeaedbb97795d6028
harrychengly <harrychengly@gmail.com>
2018-12-11 20:27:51 +0800

    connected mysql to app

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  #set index page /root url
+  root 'pages#home'
+  get 'pages/about'
+  get 'pages/home'
+  get 'pages/contact'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
